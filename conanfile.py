e2013bf2cf981e1d17030f8d039c6cbd3d5fb867
danimtb <danimanzaneque@gmail.com>
2018-03-07 12:02:21 +0100

    Updated conanfiles and fixed version to 0.6.2

--- conanfile.py
+++ conanfile.py
@@ -3,30 +3,32 @@ import os
 
 class DocoptConan(ConanFile):
     name = "docopt"
-    version = "master"
+    version = "0.6.2"
     license = "MIT and Boost"
-    url = "https://github.com/memsharded/conan-docopt.git"
+    url = "https://github.com/docopt/docopt.cpp"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
     default_options = "shared=False"
     generators = "cmake"
 
     def source(self):
-        self.run("git clone https://github.com/docopt/docopt.cpp.git")
-        tools.replace_in_file("docopt.cpp/CMakeLists.txt", "include(GNUInstallDirs)", """include(GNUInstallDirs)
+        # https://github.com/docopt/docopt.cpp/archive/v0.6.2.zip
+        tools.get("%s/archive/v%s.zip" % (self.url, self.version))
+        os.rename("docopt.cpp-%s" % self.version, "sources")
+        tools.replace_in_file("sources/CMakeLists.txt", "include(GNUInstallDirs)", """include(GNUInstallDirs)
 include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
 conan_basic_setup()
 """)
 
     def build(self):
-        cmake = CMake(self.settings)
-        self.run('cmake docopt.cpp %s' % cmake.command_line)
-        self.run("cmake --build . %s" % cmake.build_config)
+        cmake = CMake(self)
+        cmake.configure(source_folder="sources")
+        cmake.build()
 
     def package(self):
-        self.copy("docopt.h", "include", "docopt.cpp")
-        self.copy("docopt_value.h", "include", "docopt.cpp")
-        self.copy("docopt_util.h", "include", "docopt.cpp")
+        self.copy("docopt.h", "include", "sources")
+        self.copy("docopt_value.h", "include", "sources")
+        self.copy("docopt_util.h", "include", "sources")
         self.copy("*docopt_s.lib", "lib", keep_path=False)
         self.copy("*docopt.lib", "lib", keep_path=False)   
         if self.options.shared:
@@ -40,5 +42,3 @@ conan_basic_setup()
             self.cpp_info.libs = ["docopt"]
         else:
             self.cpp_info.libs = ["docopt_s"]
-        
-
